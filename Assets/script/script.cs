79e974569140bbe338abb88e73cff664198103e7
ThanawatRungrueng <126415878+ThanawatRungrueng@users.noreply.github.com>
2024-08-03 20:30:35 -0700

    up2

---
+++ Assets/script/script.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class script : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    int forcePower;
+
+    [SerializeField]
+    private float xpower;
+    [SerializeField]
+  
+    float Increment;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+     MoveLeft();
+       
+    }
+    public void MoveLeft()
+    {
+        xpower = Input.GetAxis("Horizontal");
+        rb.AddForce(xpower * Vector3.right * forcePower);
+    }
+  
+ 
+}
