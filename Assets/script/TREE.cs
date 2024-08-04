64ebfb6cc618dd2843a88200de5d6313db55af80
ThanawatRungrueng <126415878+ThanawatRungrueng@users.noreply.github.com>
2024-08-03 21:27:47 -0700

    hp

--- Assets/script/TREE.cs
+++ Assets/script/TREE.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class TREE : MonoBehaviour
 {
+    private MeshRenderer rd;
+        public void Start()
+    {
+        rd = GetComponent<MeshRenderer>();  
+    }
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
+        rd.material.color = Color.red;
         script player = collision.gameObject.GetComponent<script>();
         player.HP -= 15;
 
@@ -16,6 +22,10 @@ public class TREE : MonoBehaviour
             player.HP = 0;
             Main_UI.instance.ShowNotiText("you gay");
         }    
-       
+      
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        rd.material.color = new Color32(152, 76, 2, 255);
     }
 }
